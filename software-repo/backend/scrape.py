6ceef65a63fb98c971af708d55da5a328926df9c
Steven Yeu <syeu@sdsc.edu>
2021-12-10 17:52:55 -0800

    updated scraping to include tools from python package

--- software-repo/backend/scrape.py
+++ software-repo/backend/scrape.py
@@ -9,9 +9,18 @@ def scrape_tools():
     soup = BeautifulSoup(response.content, "html.parser")
     tool_list = soup.find(class_="toollist")
     tools = tool_list.find_all("li")
-    # print(tools[0].contents[1].find("a").string.strip().split(" ")[0])
+    python_tool = [
+        tool for tool in tools if tool.strong.a.string == "Python on Expanse"
+    ]
+    python_tool_list = python_tool[0].contents[-1].strip()
+    start_index = python_tool_list.find(":") + 1
+    python_tool_list = python_tool_list[start_index:].split(",")
     tools = [tool.contents[1].find("a").string.strip().split(" ")[0] for tool in tools]
     unique_tools = [
         tool for tool in tools if tool != "DEBUG" and not bool(re.search(r"\d", tool))
     ]
-    return {"tools": unique_tools}
+
+    return {"tools": unique_tools + python_tool_list}
+
+
+print(scrape_tools())
